c65bdbe5f4d263e964150f5ac9b5b14df1ce5a9d
root <root@2f7e45884f12>
2020-05-06 19:00:22 +0000

    Camp Day 3

--- src/section3/MadlibsOfTheAmazon.java
+++ src/section3/MadlibsOfTheAmazon.java
@@ -8,16 +8,32 @@ public class MadlibsOfTheAmazon {
 		// Put this sentence in a pop up:
 		// If you find yourself having to cross a piranha-infested river, here's how to do it...
 		
+	  JOptionPane.showMessageDialog(null, "If you find yourself having to cross a piranha-infested river, here's how to do it..");
 		// Get the user to enter an adjective
+		String adjective = JOptionPane.showInputDialog(null, "Enter a adjective");
 
 		// Get the user to enter a type of liquid
+		String liquid = JOptionPane.showInputDialog(null, "Enter a liquid");
+		
 
 		// Get the user to enter a body part
+		String bodyPart = JOptionPane.showInputDialog(null, "Enter a body part");
 
 		// Get the user to enter a verb
+		String verb = JOptionPane.showInputDialog(null, "Enter a verb");
 
 		// Get the user to enter a place
+		String place = JOptionPane.showInputDialog(null, "Enter a place");
 
+		String adLib = "Piranhas are more "+adjective+" during the day, so cross the river at\n" +
+			"night.  Piranhas are attracted to fresh "+liquid+" and will most\n"+
+		"likely take a bite out of your "+bodyPart+" if you "+verb+"\n."+
+		"Whatever you do, if you have an open wound, try to find another way to get\n"+
+		"back to the "+place+". Good luck!";
+		JOptionPane.showMessageDialog(null, adLib);
+		
+		
+        
 		// Fit the user's words into this sentence, and save it in a String:
 		// Piranhas are more [adjective] during the day, so cross the river at
 		// night. Piranhas are attracted to fresh [type of liquid] and will most
--- src/section4/RobotTreasureHunt.java
+++ src/section4/RobotTreasureHunt.java
@@ -16,45 +16,63 @@ import org.jointheleague.graphical.robot.Robot;
 public class RobotTreasureHunt implements KeyEventDispatcher{
 
 	// 1. Create a new mini robot (type "mini" inside the parentheses)
+	Robot walle = new Robot ("mini");
 	
 	private void goUp() throws InterruptedException {
 		// 2. Make the robot move up the screen (use setAngle(angle) and microMove(distance))
-		
+		walle.setAngle(0);
+		walle.microMove(100);
 	}
 
 	private void goDown() throws InterruptedException{
 		// 3. make the robot move down the screen (use setAngle(angle) and microMove(distance))
-		
+	
+	walle.setAngle(180);
+		walle.microMove(100);
 	}
 
 	private void turnLeft() throws InterruptedException{
 		// 4. Make the robot turn to the left (use setAngle(angle) and microMove(distance))
-
+walle.setAngle(270);
+walle.microMove(100);
 	}
 
 	private void turnRight() throws InterruptedException{
 		// 5. make the robot turn to the right (use setAngle(angle) and microMove(distance))
-		
+		walle.setAngle(90);
+		walle.microMove(100);
 	}
 
 	private void spaceBarWasPressed() {
 
-		// 5. Change ROBOTNAME below to match the name of the robot you created in step 1.  THEN, remove the slashes at the beginning of the next two lines
-		//int robotXLocation = ROBOTNAME.getX();
-		//int robotYLocation = ROBOTNAME.getY();
+		// 5.2 Change ROBOTNAME below to match the name of the robot you created in step 1.  THEN, remove the slashes at the beginning of the next two lines
+		int robotXLocation = walle.getX();
+		int robotYLocation = walle.getY();
 		
 		// 6. Print the robotXLocation and robotYLocation variables to the console 
-		
+		System.out.println("X:"+ robotXLocation);
+		System.out.println("Y:"+ robotYLocation);
 		// 7. If robot is at same location as the little girl
 		//      --make a pop-up tell the robot where to go next
+		if (robotXLocation==750 && robotYLocation==400)
+		{
 		
+		
+		JOptionPane.showMessageDialog(null, "The next clue is with the boy in the blue cap");
+		}
 		// 8. Give the user subsequent clues at different locations on the image
 		// (pirate robot, swamp, parrots, etc.)
-		
+		if (robotXLocation==250 && robotYLocation==400)
+		{
+			JOptionPane.showMessageDialog(null, "Go to where the birds rest");
+		}
 		// 9.  If the robot is in the final location
 		//     --call the treasureFound() method
-		
+		if(robotXLocation==650 && robotYLocation==100) {
+			JOptionPane.showMessageDialog(null, "You found the treasure !!.");
+		}
 	}
+
 
 	private void go() {
 		KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(this);
